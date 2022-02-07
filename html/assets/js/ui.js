cafcdce40d120ede8d0ec8e6f279fd4e4734f533
designModiila <design@modiila.com>
2022-02-07 09:06:50 +0900

    20220207 update

--- html/assets/js/ui.js
+++ html/assets/js/ui.js
@@ -1,25 +1,25 @@
 
 $(document).ready(function(){
-  console.log('시작');
+  //console.log('시작');
   $('#fullpage').fullpage({
     verticalCentered: true,
     autoScrolling: false,
     //paddingTop: 100,
 
     onLeave : function (index, nextIndex, direction){
-      console.log('아래');
+      //console.log('아래');
         if (index == 1 && direction == 'down'){
-          console.log('aaa-1');
+          //console.log('aaa-1');
             $(".header").addClass("reverse");
             $(".btnTop").show();
         }else if (index == 2 && direction == 'up'){
-            console.log('aaa-2');
+            //console.log('aaa-2');
             $(".header").removeClass("reverse");
 
         }
     },
     afterLoad: function (anchorLink, index) {
-      console.log('aaa-3');
+      //console.log('aaa-3');
       if (index == 1){
         $("#header").removeClass("reverse");
         $.fn.fullpage.setAllowScrolling(true);
