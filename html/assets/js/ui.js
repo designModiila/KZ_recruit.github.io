dc9eae244b13fb0ccc85ec8455a2ee27143dc3dc
rosemary-HEO <rosemary@modiila.com>
2022-02-16 03:38:18 +0900

    채용공고 화면 마크업

--- html/assets/js/ui.js
+++ html/assets/js/ui.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
         }else if (index == 2 && direction == 'up'){
             //console.log('aaa-2');
             $(".header").removeClass("reverse");
+            $(".btnTop").hide();
 
         }
     },
@@ -28,16 +29,60 @@ $(document).ready(function(){
       }
     }
 
-   });
+  });
     // fullpage
 
-    $(".btnTop .btn").on('click',function(){
-        $.fn.fullpage.moveTo(1);
 
-        var timer = setInterval(function() {
-           $(".header").removeClass("reverse");
-           clearInterval(timer);
-         }, 1000);
-    });
+$(".sortOption button").click(function() {
+    $(this).siblings().removeClass('active');  
+    $(this).addClass('active');
+
+  // if ($(this).siblings().removeClass("active")) {
+  //   $(".announceList.cardTypeR").removeClass("cardTypeR");
+  //   $(".announceList").addClass("listTypeR");
+
+  
+  // }
+  // else {
+  //   $(".announceList.listTypeR").removeClass("listTypeR");
+  //   $(".announceList").addClass("cardTypeR");
+
+    
+
+});
+
+
+
+$( ".listTypeR li" ).each(function() {
+  var total = position + positionW + positionW2 + 25;
+  var position = $(".listTypeR .articleWrap .badge").position().left;
+  var positionW = $(".listTypeR .articleWrap .badge").outerWidth();
+  var positionW2 = $(".listTypeR .articleWrap .companyName").outerWidth();
+
+  $(".tagBox").css("left", total);
+  // var percent = 20 * (i+1) +'%'; 
+  // $( this ).css( "left", "+=" + percent ); 
+});
+
+
+
+// $(".listTypeR li").each(function(index) { 
+//   var position = $(".listTypeR .articleWrap .badge").position().left;
+//   var positionW = $(".listTypeR .articleWrap .badge").outerWidth();
+//   var positionW2 = $(".listTypeR .articleWrap .companyName").outerWidth();
+
+//   $(".tagBox").css("left", position + positionW + positionW2 + 25);
+// });
+  // $(".btnTop .btn").on('click',function(){
+  //     $.fn.fullpage.moveTo(1);
+  //     $(".btnTop").hide();
+
+      // var timer = setInterval(function() {
+      //     $(".header").removeClass("reverse");
+      //     clearInterval(timer);
+      //   }, 1000);
+  // });
+
+
 
 });
