eb74dde1b411cb42ebd21fe00acfdf4e3ec7a038
rosemary-HEO <rosemary@modiila.com>
2022-02-21 21:46:03 +0900

    채용공고 화면수정

--- html/assets/js/ui.js
+++ html/assets/js/ui.js
@@ -1,37 +1,18 @@
 
 $(document).ready(function(){
-  //console.log('시작');
-  $('#fullpage').fullpage({
-    verticalCentered: true,
-    autoScrolling: false,
-    //paddingTop: 100,
+ 
 
-    onLeave : function (index, nextIndex, direction){
-      //console.log('아래');
-        if (index == 1 && direction == 'down'){
-          //console.log('aaa-1');
-            $(".header").addClass("reverse");
-            $(".btnTop").show();
-        }else if (index == 2 && direction == 'up'){
-            //console.log('aaa-2');
-            $(".header").removeClass("reverse");
-            $(".btnTop").hide();
-
-        }
-    },
-    afterLoad: function (anchorLink, index) {
-      //console.log('aaa-3');
-      if (index == 1){
-        $("#header").removeClass("reverse");
-        $.fn.fullpage.setAllowScrolling(true);
-        $(".btnTop").hide();
-
-      }
-    }
-
-  });
-    // fullpage
-
+$(".accordionHeader").on("click", function() {
+  if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).siblings(".accordionContent").slideUp(200);
+  } else {
+      $(".accordionHeader").removeClass("active");
+      $(this).addClass("active");
+      $(".accordionContent").slideUp(200);
+      $(this).siblings(".accordionContent").slideDown(200);
+  }
+});    
 
 $(".sortOption button").click(function() {
     $(this).siblings().removeClass('active');  
@@ -49,6 +30,58 @@ $(".sortOption button").click(function() {
 
     
 
+});
+
+
+let selectCustomFlag;
+$('.customSelectBox').on('click', function() {
+  $(this).toggleClass('selected');
+  if($(this).hasClass('selected')) {
+    $('.customSelectList').show();
+  } else {
+    $('.customSelectList').hide();
+  }
+})
+
+$('.customSelectBox').on('focusin', function() {
+  $('.customSelectList').show();
+});
+
+$('.customSelectBox').on('focusout', function() {
+  if(!selectCustomFlag) {
+    $('.customSelectList').hide();
+  }
+  $(this).removeClass('selected');
+});
+
+$('.customSelectOption').on('mouseenter', function() {
+  selectCustomFlag = true;
+ 
+});
+
+$('.customSelectOption').on('mouseout', function() {
+  selectCustomFlag = false;
+});
+
+$('.customSelectOption').on('click', function() {
+  let value = $(this).attr('value');
+  $(".customSelectOption").removeClass("selected");
+  $(this).addClass("selected");
+  $('.customSelectText').text($(this).text());
+  $('.selectOrigin').val(value);
+  $('.customSelectList').hide();
+
+  $('.selectOrigin').find('option').each(function(index, el) {
+    if($(el).attr('value') == value) {
+      $(el).attr('selected', 'selected');
+    } else {
+      $(el).removeAttr('selected');
+    }
+  });
+});
+
+$('.inputFile').on('click', 'button',function() {
+  $(this).closest('.inputFile').find('input[type="file"]').click(); 
 });
 
 
@@ -83,6 +116,38 @@ $( ".listTypeR li" ).each(function() {
       //   }, 1000);
   // });
 
+
+   //console.log('시작');
+  //  $('#fullpage').fullpage({
+  //   verticalCentered: true,
+  //   autoScrolling: false,
+  //   //paddingTop: 100,
+
+  //   onLeave : function (index, nextIndex, direction){
+  //     //console.log('아래');
+  //       if (index == 1 && direction == 'down'){
+  //         //console.log('aaa-1');
+  //           $(".header").addClass("reverse");
+  //           $(".btnTop").show();
+  //       }else if (index == 2 && direction == 'up'){
+  //           //console.log('aaa-2');
+  //           $(".header").removeClass("reverse");
+  //           $(".btnTop").hide();
+
+  //       }
+  //   },
+  //   afterLoad: function (anchorLink, index) {
+  //     //console.log('aaa-3');
+  //     if (index == 1){
+  //       $("#header").removeClass("reverse");
+  //       $.fn.fullpage.setAllowScrolling(true);
+  //       $(".btnTop").hide();
+
+  //     }
+  //   }
+
+  // });
+    // fullpage
 
 
 });
